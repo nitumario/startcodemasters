1961492e1c037adc08d53d7a5ac9a35785c2cd5f
fllstartcodeMasters <fll.startcode@gmail.com>
2023-11-26 12:57:56 +0200

    restructurare completa a codului

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env pybricks-micropython
 
-#! Nu modificati importurile ca ne ducem dracu sau sa modificati orice alt ceva infara de runuri
-#! version: 0.3
 #***************************IMPORTS***************************
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -16,42 +14,81 @@ import _thread
 
 #***************************VALUES***************************
 
-# Create variables
+#definim caramida si display-ul
 zap=EV3Brick()
-
-viteza = 350 #viteza
-vitneg = -viteza #viteza negativa
-vitezafin = 400 #viteza final
-acceleratie=200
-vitmax=1000
 zapdisplay = EV3Brick()
 
-# definim motoarele si senzorii
+#definim motoarele si senzorii
 st = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 dr = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 bratSt = Motor(Port.A)
 bratDr = Motor(Port.D)
-touch_sensor = TouchSensor(Port.S3)
-left_sensor = ColorSensor(Port.S2)
 right_sensor = ColorSensor(Port.S1)
-#gyro = GyroSensor(Port.S3)
+left_sensor = ColorSensor(Port.S2)
+touch_sensor = TouchSensor(Port.S3)
+gyro = GyroSensor(Port.S4)
 
-# cream drivebase-ul
+#cream setarile robotului
 zap1 = DriveBase(st, dr, 50, 105)
-global coefd1
-global coeft1
-coefd1 = 1
-coeft1 = 1
+zap2 = DriveBase(st, dr, 50, 105)
+zap3 = DriveBase(st, dr, 50, 105)
+zap4 = DriveBase(st, dr, 50, 105)
+zap5 = DriveBase(st, dr, 50, 105)
+zap6 = DriveBase(st, dr, 50, 105)
+zap7= DriveBase(st, dr, 50, 105)
+zap8 = DriveBase(st, dr, 50, 105)
+
 zap1.settings(800, 500, 300, 300)
+zap2.settings(800, 500, 300, 300)
+zap3.settings(800, 500, 300, 300)
+zap4.settings(800, 500, 300, 300)
+zap5.settings(800, 500, 300, 300)
+zap6.settings(800, 500, 300, 300)
+zap7.settings(800, 500, 300, 300)
+zap8.settings(800, 500, 300, 300)
+
 st.stop()
 dr.stop()
-global varBrat
-varBrat = 0
-sem = _thread.allocate_lock()
-zap.speaker.beep() 
+#cream coeficientii pentru erori
+global coefd1
+global coeft1
+global coefd2
+global coeft2
+global coefd3
+global coeft3
+global coefd4
+global coeft4
+global coefd5
+global coeft5
+global coefd6
+global coeft6
+global coefd7
+global coeft7
+global coefd8
+global coeft8
+
+coefd1 = 1
+coeft1 = 1
+coefd2 = 1
+coeft2 = 1
+coefd3 = 1
+coeft3 = 1
+coefd4 = 1
+coeft4 = 1
+coefd5 = 1
+coeft5 = 1
+coefd6 = 1
+coeft6 = 1
+coefd7 = 1
+coeft7 = 1
+coefd8 = 1
+coeft8 = 1
 
 #***************************THREAD BRATE***************************
 
+global varBrat
+varBrat = 0
+sem = _thread.allocate_lock()
 def brat01_thread():
     global varBrat
     while True:
@@ -90,70 +127,37 @@ varBrat = 0
 sem.release()
 
 #***************************RUNS***************************
+#bratDr (+) ridicare
+#bratSt (-) ridicare
+
 def run01():
+    zap1.straight(-400)
+    time.sleep(0.2)
+    zap1.straight(400)
+
+def run02():
     bratDr.run_time(500,180)
     zap1.straight(coefd1*40)
     zap1.turn(coeft1*-50)
     zap1.straight(coefd1*190)
-    
     zap1.turn(coeft1*-60)
     zap1.straight(coefd1*60)
     bratDr.run_time(700,500)
     zap1.turn(coeft1*72)
     zap1.straight(coefd1*-60)
-    #zap1.straight(coefd1*120)
-    #bratDr.run_time(450, 600)
-    #zap1.straight(coefd1*60)
-    #zap1.turn(coeft1*-10)
-    #zap1.straight(coefd1*70)
-
-    #bratSt.run_time(-1000, 6000)
-
-
-    #zap1.straight(coefd1*200)
-    #zap1.turn(coeft1*-23)
-    #zap1.straight(coefd1*40)
-    #zap1.turn(coeft1*30)
-    #zap1.straight(coefd1*50)
-    #zap1.turn(coeft1*-7)
-    #bratDr.run_time(450, 550)
-    #zap1.straight(coefd1*200)
 
     bratDr.stop()
     bratSt.stop()
 
-def run02():
+def run03():
     zap1.straight(coefd1*500)
     zap1.turn(coeft1*37)
     zap1.straight(coefd1*240) 
     zap1.turn(coeft1*-55)
     zap1.straight(coefd1*100)
 
-
     bratDr.stop()
     bratSt.stop()
-    
-def run03():
-    zap1.straight(-400)
-    time.sleep(0.2)
-    zap1.straight(400)
-
-
-def run05(degrees):
-    st.reset_angle(0)
-    while st.angle() < degrees:
-        zap1.straight(coefd1*50)
-        time.sleep(0.1)
-        zapdisplay.screen.clear()
-        zapdisplay.screen.draw_text(80, 50, str(st.angle()), Color.BLACK, None)
-        if left_sensor.color() == Color.WHITE:
-            zap1.turn(10)
-        if left_sensor.color() == Color.BLACK:
-            zap1.turn(-10)
-
-
-#bratDr (-) ridicare
-#bratSt (+) ridicare
 
 def run06():
     #ridica bratele
@@ -180,27 +184,38 @@ def run06():
     bratDr.run_time(-400 ,550)
     #merge in baza
     zap1.straight(coefd1*400)
+
     bratDr.stop()
     bratSt.stop()
 
+def urmarireLinie1(degrees):
+    st.reset_angle(0)
+    while st.angle() < degrees:
+        zap1.straight(coefd1*50)
+        time.sleep(0.1)
+        zapdisplay.screen.clear()
+        zapdisplay.screen.draw_text(80, 50, str(st.angle()), Color.BLACK, None)
+        if left_sensor.color() == Color.WHITE:
+            zap1.turn(10)
+        if left_sensor.color() == Color.BLACK:
+            zap1.turn(-10)
+
 
 #***************************Display***************************
-
+#afisare run pe ecran
 x = 1
 zapdisplay.screen.draw_text(80, 50, str(x), Color.BLACK, None) 
+zap.speaker.beep() 
 
-
-# afisare run pe ecran
 def update_screen(x):
     zapdisplay.screen.clear()
     zapdisplay.screen.draw_text(80, 50, str(x), Color.BLACK, None)
 
 touch=0
 
-# main
+#main
 while True:
-    
-    # verificare apasare butoane
+    #verificare apasare butoane
     if Button.UP in zapdisplay.buttons.pressed() and x < 8:
         x = x+1
         update_screen(x)
@@ -210,7 +225,6 @@ while True:
         x = x-1
         update_screen(x)
         wait(700)
-
 
     if int(x)==1 and touch_sensor.pressed() :
         touch = 1
