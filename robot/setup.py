26257579bf319482f1fecdf57ba3fee63ccd4f5f
fllstartcodeMasters <149675286+fllstartcodeMasters@users.noreply.github.com>
2024-02-24 01:50:33 +0200

    Update setup.py
    functii senzori culoare

--- robot/setup.py
+++ robot/setup.py
@@ -1,3 +1,6 @@
+
+#************IMPORTS************
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -8,19 +11,22 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import time
 import _thread
 
+#************ROBOT CLASS************
+
 class Robot:
-    def __init__(self, wheel_diameter, axle_track, ):
+    def __init__(self, wheel_diameter: float, axle_track: float):
         senzorfail = 0
         self.wheel_diameter = wheel_diameter
         self.axle_track = axle_track
         self.ev3 = EV3Brick()
 
         while True:
+            #! Verifica motoarele si senzorii
             try:
                 self.st = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
             except:
                 self.st = None
-                print("Motor stanga, port B nu a fost gasit")
+                print("Motor self.stanga, port B nu a fost gasit")
                 senzorfail += 1
 
             try:
@@ -34,7 +40,7 @@ class Robot:
                 self.bratSt = Motor(Port.A)
             except:
                 self.bratSt = None
-                print("Motor brat stanga, port A nu a fost gasit")
+                print("Motor brat self.stanga, port A nu a fost gasit")
                 senzorfail += 1
 
             try:
@@ -55,7 +61,7 @@ class Robot:
                 self.senzorCuloareSt = ColorSensor(Port.S2)
             except:
                 self.senzorCuloareSt = None
-                print("Senzor culoare stanga, port S2 nu a fost gasit")
+                print("Senzor culoare self.stanga, port S2 nu a fost gasit")
                 senzorfail += 1
 
             try:
@@ -77,60 +83,59 @@ class Robot:
                 wait(2000)
             break
 
-
-
         self.d = DriveBase(self.st, self.dr, self.wheel_diameter, self.axle_track)
         self.coefd = 1
         self.coeft = 1
+
+    # THREAD-uri
 
     lock0 =_thread.allocate_lock()
     lock1 = _thread.allocate_lock()
     lock2 = _thread.allocate_lock()
     lock3 = _thread.allocate_lock()
 
-    def straight_threadSET(self, angles,lock):
+    def self.straight_threadSET(self, angles: int, lock) -> None:
         with lock:
-            self.d.straight(self.coefd * angles)
+            self.d.self.straight(self.coefd * angles)
 
-    def run_by_time_threadSET(self,speed,time,motor,lock):
+    def run_by_time_threadSET(self, speed: int, time, motor, lock) -> None:
         with lock:
             motor.run_time(speed, time)
 
-    def run_by_angle_threadSET(self,speed,angle,motor,lock):
+    def run_by_angle_threadSET(self, speed: int, angle: int, motor, lock) -> None:
         with lock:
             motor.run_angle(speed, angle)
     global threadstop
     threadstop = False
-    def thread_start(self):
+    def thread_self.start(self):
         threadstop = False
     def thread_stop(self):
         threadstop = True
 
-    def straight_thread(self, angles, lock):
+    def self.straight_thread(self, angles, lock) -> None:
         if not threadstop:
-            _thread.start_new_thread(self.straight_threadSET, (angles,lock))
+            _thread.self.start_new_thread(self.self.straight_threadSET, (angles,lock))
 
-    def run_by_time_thread(self, speed,time,motor, lock):
+    def run_by_time_thread(self, speed, time, motor, lock) -> None:
         if not threadstop:
-            _thread.start_new_thread(self.run_by_time_threadSET, (speed,time,motor,lock))
+            _thread.self.start_new_thread(self.run_by_time_threadSET, (speed,time,motor,lock))
 
-    def run_by_angle_thread(self, speed,angle,motor, lock):
+    def run_by_angle_thread(self, speed, angle, motor, lock) -> None:
         if not threadstop:
-            _thread.start_new_thread(self.run_by_angle_threadSET, (speed,angle,motor,lock))
+            _thread.self.start_new_thread(self.run_by_angle_threadSET, (speed,angle,motor,lock))
 
-    
-            
-
-    def datalog(self, data_logger):
+    def datalog(self, data_logger) -> None:
         if self.RecordOn is True: data_logger=True
 
         if data_logger is True:
-            self.datalog.log(self.timer.time(), self.d.distance(),self.senzorGiro.angle(), self.senzorCuloareSt.reflection(),self.senzorCuloareDr.reflection(), \
+            self.datalog.log(self.timer.time(), self.d.diself.stance(),self.senzorGiro.angle(), self.senzorCuloareSt.reflection(),self.senzorCuloareDr.reflection(), \
                             self.bratSt.speed(), self.bratDr.speed(), self.st.speed(), self.dr.speed())
         else:
             pass
         
-    def gyrogoto(self, scop, viteza, abandon_timer = 4000):
+    #************GYRO FUNCTIONS************
+
+    def gyrogoto(self, scop, viteza: int, abandon_timer = 4000) -> None:
         self.d.stop()
         timer = StopWatch()
         while(timer.time()<abandon_timer):
@@ -169,3 +174,110 @@ class Robot:
                 print(self.senzorGiro.angle())
             self.st.hold()
             self.dr.hold()
+
+    #************COLOUR FUNCTIONS************
+
+    self.StR = 0
+    self.DrR = 0
+
+    def gasireRgb(self.StR, self.DrR) -> None:
+        if self.StR == 1 and self.DrR == 0:
+            print(senzorCuloareSt.rgb())
+            wait(100)
+        elif self.StR == 0 and self.DrR == 1:
+            print(senzorCuloareDr.rgb())
+            wait(100)
+
+    def gasireCuloare(self.StC, self.DrC) -> None:
+        if self.StC == 1 and self.DrC == 0:
+            print(senzorCuloareSt.color())
+            wait(100)
+        elif self.StC == 0 and self.DrC == 1:
+            print(senzorCuloareDr.color())
+            wait(100)
+
+    def gasireLuminaAmbient(self.StA, self.DrA) -> None:
+        if self.StA == 1 and self.DrA == 0:
+            print(senzorCuloareSt.ambient())
+            wait(100)
+        elif self.StA == 0 and self.DrA == 1:
+            print(senzorCuloareDr.ambient())
+            wait(100)
+
+    def gasireRefractie(self.StRe, self.DrRe) -> None:
+        if self.StRe == 1 and self.DrRe == 0:
+            print(senzorCuloareSt.reflection())
+            wait(100)
+        elif self.StRe == 0 and self.DrRe == 1:
+            print(senzorCuloareDr.reflection())
+            wait(100)
+
+    def oprireRgb(self.StR, self.DrR, r, g, b) -> None:
+        self.d.st.reset_angle(0)
+        rgbul = [r, g, b]
+        while True:
+            if self.StR == 1 and self.DrR == 0:
+                while senzorCuloareSt.rgb()!=rgbul:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            while self.StR == 0 and self.DrR == 1:
+                while senzorCuloareDr.rgb()!=rgbul:
+                    st.run(300)
+                    dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            break
+
+    def oprireCuloare(self.StC, self.DrC, unghiRoata, culoare) -> None:
+        self.d.st.reset_angle(0)
+        while True:
+            if self.StC == 1 and self.DrC == 0:
+                while senzorCuloareSt.color()!=culoare and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            elif self.StC == 0 and self.DrC == 1:
+                while senzorCuloareDr.color()!=culoare and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            break
+
+    def oprireLuminaAmbient(self.StA, self.DrA, unghiRoata, luminaAmbient) -> None:
+        self.d.st.reset_angle(0)
+        while True:
+            if self.StC == 1 and self.DrC == 0:
+                while senzorCuloareSt.ambient()!=luminaAmbient and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            elif self.StC == 0 and self.DrC == 1:
+                while senzorCuloareDr.ambient()!=luminaAmbient and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            break
+
+    def oprireRefractie(self.StRe, self.DrRe, unghiRoata, refractie) -> None:
+        self.d.st.reset_angle(0)
+        while True:
+            if self.StC == 1 and self.DrC == 0:
+                while senzorCuloareSt.reflection()!=refractie and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            elif self.StC == 0 and self.DrC == 1:
+                while senzorCuloareDr.reflection()!=refractie and st.angle()<unghiRoata:
+                    self.d.st.run(300)
+                    self.d.dr.run(300)
+                self.d.st.hold()
+                self.d.dr.hold()
+            break
+
