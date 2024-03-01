768a94ab0d05bd08edb3e0438b237e21a5290248
Nitu Mario <50844782+nitumario@users.noreply.github.com>
2024-03-02 01:38:47 +0200

    Update setup.py

--- robot/setup.py
+++ robot/setup.py
@@ -10,9 +10,10 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import time
 import _thread
+import numpy as np
+import matplotlib.pyplot as plt
 
 #************ROBOT CLASS************
-
 class Robot:
     def __init__(self, wheel_diameter: float, axle_track: float):
         senzorfail = 0
@@ -93,8 +94,13 @@ class Robot:
     lock1 = _thread.allocate_lock()
     lock2 = _thread.allocate_lock()
     lock3 = _thread.allocate_lock()
+    threadstop = False
+    def thread_start(self):
+        self.threadstop = False
+    def thread_stop(self):
+        self.threadstop = True
 
-    def self.straight_threadSET(self, angles: int, lock) -> None:
+    def straight_threadSET(self, angles: int, lock) -> None:
         with lock:
             self.d.self.straight(self.coefd * angles)
 
@@ -105,183 +111,120 @@ class Robot:
     def run_by_angle_threadSET(self, speed: int, angle: int, motor, lock) -> None:
         with lock:
             motor.run_angle(speed, angle)
-    global threadstop
-    threadstop = False
-    def thread_self.start(self):
-        threadstop = False
-    def thread_stop(self):
-        threadstop = True
 
-    def self.straight_thread(self, angles, lock) -> None:
-        if not threadstop:
+    def straight_thread(self, angles, lock) -> None:
+        if not self.threadstop:
             _thread.self.start_new_thread(self.self.straight_threadSET, (angles,lock))
 
     def run_by_time_thread(self, speed, time, motor, lock) -> None:
-        if not threadstop:
+        if not self.threadstop:
             _thread.self.start_new_thread(self.run_by_time_threadSET, (speed,time,motor,lock))
 
     def run_by_angle_thread(self, speed, angle, motor, lock) -> None:
-        if not threadstop:
+        if not self.threadstop:
             _thread.self.start_new_thread(self.run_by_angle_threadSET, (speed,angle,motor,lock))
 
     def datalog(self, data_logger) -> None:
-        if hasattr(self, "RecordOn") and (self.RecordOn is True): 
+        if self.RecordOn is True: data_logger=True
+
+        if data_logger is True:
             self.datalog.log(self.timer.time(), self.d.diself.stance(),self.senzorGiro.angle(), self.senzorCuloareSt.reflection(),self.senzorCuloareDr.reflection(), \
                             self.bratSt.speed(), self.bratDr.speed(), self.st.speed(), self.dr.speed())
-            
-        #if self.RecordOn is True: data_logger=True
-
-        #if data_logger is True:
-        #    self.datalog.log(self.timer.time(), self.d.diself.stance(),self.senzorGiro.angle(), self.senzorCuloareSt.reflection(),self.senzorCuloareDr.reflection(), \
-        #                    self.bratSt.speed(), self.bratDr.speed(), self.st.speed(), self.dr.speed())
-        #else:
-        #    pass
+        else:
+            pass
         
     #************GYRO FUNCTIONS************
 
-    def gyrogoto(self, scop, viteza: int, abandon_timer = 4000) -> None:
+    def gyrogoto(self, scop, viteza: int, abandon_timer=4000, spin=1) -> None:
         self.d.stop()
         timer = StopWatch()
-        while(timer.time()<abandon_timer):
-            print("timp",timer.time())
-            while self.senzorGiro.angle() < scop:
-                self.st.run(viteza)
-                self.dr.run(-viteza)
-                print(self.senzorGiro.angle())
-            self.st.hold()
-            self.dr.hold()
-            if scop-self.senzorGiro.angle() > 10:
-                print("a iesit1")
-                self.d.turn(self.coeft*scop-self.senzorGiro.angle())
-                break
-            elif scop-self.senzorGiro.angle() < -10:
-                print("a iesit2")
-                self.d.turn(self.coeft*scop-self.senzorGiro.angle())
-                break
-            self.st.hold()
-            self.dr.hold()
+        if spin == 1:
+            while timer.time() < abandon_timer:
+                print("timp", timer.time())
+                while self.senzorGiro.angle() < scop:
+                    self.st.run(viteza)
+                    self.dr.run(-viteza)
+                    print(self.senzorGiro.angle())
+                    if timer.time() >= abandon_timer:
+                        print("Abandoning due to timeout")
+                        self.st.hold()
+                        self.dr.hold()
+                        return
+                self.st.hold()
+                self.dr.hold()
+                if scop - self.senzorGiro.angle() > 10:
+                    print("a iesit1")
+                    self.d.turn(self.coeft * (scop - self.senzorGiro.angle()))
+                    break
+                elif scop - self.senzorGiro.angle() < -10:
+                    print("a iesit2")
+                    self.d.turn(self.coeft * (scop - self.senzorGiro.angle()))
+                    break
+                self.st.hold()
+                self.dr.hold()
+                if scop - self.senzorGiro.angle() > 10:
+                    print("a iesit3")
+                    self.d.turn(self.coeft * (scop - self.senzorGiro.angle()))
+                    self.stop()
+                elif scop - self.senzorGiro.angle() < -10:
+                    print("a iesit4")
+                    self.d.turn(self.coeft * (scop - self.senzorGiro.angle()))
+                    self.stop()
+                while self.senzorGiro.angle() > scop:
+                    self.st.run(-viteza)
+                    self.dr.run(viteza)
+                    print(self.senzorGiro.angle())
+                    if timer.time() >= abandon_timer:
+                        print("Abandoning due to timeout")
+                        self.st.hold()
+                        self.dr.hold()
+                        return
+                self.st.hold()
+                self.dr.hold()
+            
 
-            if scop-self.senzorGiro.angle() > 10:
-                print("a iesit3")
-                self.d.turn(self.coeft*scop-self.senzorGiro.angle())
-                self.stop()
-            elif scop-self.senzorGiro.angle() < -10:
-                print("a iesit4")
-
-                self.d.turn(self.coeft*scop-self.senzorGiro.angle())
-                self.stop()
 
 
-            while self.senzorGiro.angle() > scop:
-                self.st.run(-viteza)
-                self.dr.run(viteza)
-                print(self.senzorGiro.angle())
-            self.st.hold()
-            self.dr.hold()
 
     #************COLOUR FUNCTIONS************
 
-    self.StR = 0
-    self.DrR = 0
 
-    def gasireRgb(self.StR, self.DrR) -> None:
-        if self.StR == 1 and self.DrR == 0:
-            print(senzorCuloareSt.rgb())
-            wait(100)
-        elif self.StR == 0 and self.DrR == 1:
-            print(senzorCuloareDr.rgb())
-            wait(100)
 
-    def gasireCuloare(self.StC, self.DrC) -> None:
-        if self.StC == 1 and self.DrC == 0:
-            print(senzorCuloareSt.color())
-            wait(100)
-        elif self.StC == 0 and self.DrC == 1:
-            print(senzorCuloareDr.color())
-            wait(100)
-
-    def gasireLuminaAmbient(self.StA, self.DrA) -> None:
-        if self.StA == 1 and self.DrA == 0:
-            print(senzorCuloareSt.ambient())
-            wait(100)
-        elif self.StA == 0 and self.DrA == 1:
-            print(senzorCuloareDr.ambient())
-            wait(100)
-
-    def gasireRefractie(self.StRe, self.DrRe) -> None:
-        if self.StRe == 1 and self.DrRe == 0:
-            print(senzorCuloareSt.reflection())
-            wait(100)
-        elif self.StRe == 0 and self.DrRe == 1:
-            print(senzorCuloareDr.reflection())
-            wait(100)
-
-    def oprireRgb(self.StR, self.DrR, r, g, b) -> None:
-        self.d.st.reset_angle(0)
-        rgbul = [r, g, b]
+    #************EXPERIMENTAL************
+    def selfDrive_log(self, name):
+        datalog_file = DataLog("distance", "gyro_angle", name=name, timestamp=False, extension=".csv", append=False)
+        distances = []
+        gyro_angles = []
         while True:
-            if self.StR == 1 and self.DrR == 0:
-                while senzorCuloareSt.rgb()!=rgbul:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            while self.StR == 0 and self.DrR == 1:
-                while senzorCuloareDr.rgb()!=rgbul:
-                    st.run(300)
-                    dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            break
+            dis = self.d.distance()
+            gyro_angle = self.senzorGiro.angle()
+            datalog_file.log(dis, gyro_angle)
+            distances.append(dis)
+            gyro_angles.append(gyro_angle)
+            
+            plt.scatter(distances, gyro_angles, color='blue', label='Data')
+            
+            z = np.polyfit(distances, gyro_angles, deg=5)  
+            p = np.poly1d(z)
+            plt.plot(distances, p(distances), color='red', label='Trendline')
+            
+            plt.xlabel('Distance')
+            plt.ylabel('Gyro Angle')
+            plt.legend()
+            plt.show()
 
-    def oprireCuloare(self.StC, self.DrC, unghiRoata, culoare) -> None:
-        self.d.st.reset_angle(0)
+    def self_drive(self, begin, stop, speed, kp, t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0, t7 = 0,):
         while True:
-            if self.StC == 1 and self.DrC == 0:
-                while senzorCuloareSt.color()!=culoare and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            elif self.StC == 0 and self.DrC == 1:
-                while senzorCuloareDr.color()!=culoare and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            break
-
-    def oprireLuminaAmbient(self.StA, self.DrA, unghiRoata, luminaAmbient) -> None:
-        self.d.st.reset_angle(0)
-        while True:
-            if self.StC == 1 and self.DrC == 0:
-                while senzorCuloareSt.ambient()!=luminaAmbient and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            elif self.StC == 0 and self.DrC == 1:
-                while senzorCuloareDr.ambient()!=luminaAmbient and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            break
-
-    def oprireRefractie(self.StRe, self.DrRe, unghiRoata, refractie) -> None:
-        self.d.st.reset_angle(0)
-        while True:
-            if self.StC == 1 and self.DrC == 0:
-                while senzorCuloareSt.reflection()!=refractie and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            elif self.StC == 0 and self.DrC == 1:
-                while senzorCuloareDr.reflection()!=refractie and st.angle()<unghiRoata:
-                    self.d.st.run(300)
-                    self.d.dr.run(300)
-                self.d.st.hold()
-                self.d.dr.hold()
-            break
-
+            x = self.d.distance() + 0
+            formula_unghi = t1 + t2 * x + t3 * x ** 2 + t4 * x ** 3 + t5 * x ** 4 + t6 * x ** 5 + t7 * x ** 6
+            if x <= begin or x >= stop:
+                unghi_dorit = 0
+            else:
+                unghi_dorit = formula_unghi
+            unghi_gyro = self.senzorGiro.angle()
+            eroare = unghi_dorit - unghi_gyro
+            corectie = kp * eroare
+            self.d.drive(speed, corectie)
+            if self.d.distance() >= stop:
+                break            
+    
